c017c7ff91c15160ad4b9107a973812ab0d615f4
Matheus Quost <matheusquost@gmail.com>
2024-05-27 20:42:05 -0300

    first commit

---
+++ src/main/java/io/github/mqdev/front_cursos/FrontCursosApplication.java
@@ -0,0 +1,13 @@
+package io.github.mqdev.front_cursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontCursosApplication.class, args);
+	} 
+
+}
