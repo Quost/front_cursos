ba47204fbde2339b006db38a20059b01fc8c9d99
Matheus Quost <matheusquost@gmail.com>
2024-06-03 16:46:02 -0300

    refactor: Add UUID field to CourseDTO

--- src/main/java/io/github/mqdev/front_cursos/modules/course/dto/CourseDTO.java
+++ src/main/java/io/github/mqdev/front_cursos/modules/course/dto/CourseDTO.java
@@ -1,5 +1,7 @@
 package io.github.mqdev.front_cursos.modules.course.dto;
 
+import java.util.UUID;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,6 +13,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CourseDTO {
     
+    private UUID id;
     private String name;
     private String category;
     private String status;
